88935ebb6da34de69ad26d0cc2337cb46372aa28
Jan Bernacki <releu@me.com>
2014-03-22 14:11:45 -0400

    from prod

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ SpaceNotice::Application.routes.draw do
   get "/subscribe/:token" => "subscriptions#new", as: :subscribe
 
   post "/v1/pushPackages/web.com.spacenotice" => "push_packages#show"
+  post "/v1/log" => "push_packages#log"  
+  post "/v1/devices/:token/registrations/web.com.spacenotice" => "push_packages#subscribe"
+  delete "/v1/devices/:token/registrations/web.com.spacenotice" => "push_packages#unsubscribe"
+  get "push" => "home#show"
 
   root "home#show"
 end
